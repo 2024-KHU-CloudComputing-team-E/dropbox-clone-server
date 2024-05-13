c4a4a7d4edb2132bd2fb0a09addcc52a33328191
dhwls <dhwlsgur795@khu.ac.kr>
2024-05-13 23:24:17 +0900

    refactor: delete keys about AWS S3 from uploadcontroller.js

--- src/controllers/uploadController.js
+++ src/controllers/uploadController.js
@@ -24,8 +24,10 @@ import multerS3 from "multer-s3";
 const s3 = new S3Client({
   region : 'us-east-1',
   credentials : {
-      accessKeyId : 'AKIA2UC3EIFM65KVHTOE',
-      secretAccessKey : '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'
+      accessKeyId : '생성한 버킷의 액세스 키',
+      secretAccessKey : '시크릿 액세스 키'
+      //이 key 관련 부분은 여기다가 key를 처음에 작성했었는데(...) 보안상의 이유로 github에 push가 거부되어서 일단 지웠습니다.
+      //찾아보니 env파일과 dotenv 패키지를 통해 키를 환경 변수화해서 외부에서 끌어올 수 있다고 하는데 조금 더 공부해보고 빨리 보완하겠습니다.
   }
 })
 

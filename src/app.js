c7412945dfe246a2bde971b7c41d0008e9af3060
SamRyong <zackinthebox@khu.ac.kr>
2024-05-17 23:24:06 +0900

    pre Merge

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import express from "express";
-import exampleRouter from "./routers/exampleRouter";
+import exampleRouter from "./routers/exampleRouter.js";
+import uploadRouter from "./routers/uploadRouter.js";
 
 const app = express();
 
@@ -7,5 +8,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use("/", exampleRouter);
+app.use("/api/uploadfile", uploadRouter);
 
 export default app;

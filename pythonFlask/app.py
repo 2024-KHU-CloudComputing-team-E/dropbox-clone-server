34dd6cddba1bd2ce76284740fe77a98aed267ce3
dhwls <dhwlsgur795@khu.ac.kr>
2024-05-21 22:38:39 +0900

    feat: Download created

--- pythonFlask/app.py
+++
@@ -1,7 +0,0 @@
-from flask import Flask, request, jsonify
-
-app = Flask(__name__)
-
-@app.route('/test', methods=["POST"])
-def test():
-    lists = request.args['file_name']
--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import express from "express";
 import exampleRouter from "./routers/exampleRouter.js";
-import uploadRouter from "./routers/uploadRouter.js";
+import downloadRouter from "./routers/downloadRouter.js";
 
 const app = express();
 
@@ -8,6 +8,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use("/", exampleRouter);
-app.use("/api/uploadfile", uploadRouter);
+app.use("/api/downloadfile", downloadRouter);
 
 module.exports = app;

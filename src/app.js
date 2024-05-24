5da56afd156c6232ab7d431de582c42e1336dd3f
SamRyong <zackinthebox@khu.ac.kr>
2024-05-24 20:27:55 +0900

    merge를 위해 수정

--- src/app.js
+++ src/app.js
@@ -1,17 +1,13 @@
 import express from "express";
+import exampleRouter from "./routers/exampleRouter.js";
 import uploadRouter from "./routers/uploadRouter.js";
-import googleRouter from "./routers/googleRouter.js";
-import testRouter from "./routers/authTestRouter.js";
-import logoutRouter from "./routers/logoutRouter.js";
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use("/api/login", googleRouter);
+app.use("/", exampleRouter);
 app.use("/api/uploadfile", uploadRouter);
-app.use("/test", testRouter);
-app.use("/api/logout", logoutRouter);
 
-export default app;
+module.exports = app;

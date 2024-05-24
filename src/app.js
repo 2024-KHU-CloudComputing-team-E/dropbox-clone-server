2abf85e2f702c06541508c5382777793ff5aee37
dhwls <dhwlsgur795@khu.ac.kr>
2024-05-24 23:55:11 +0900

    feat: download created

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import exampleRouter from "./routers/exampleRouter.js";
 import uploadRouter from "./routers/uploadRouter.js";
+import downloadRouter from "./routers/downloadRouter.js";
 
 const app = express();
 
@@ -9,5 +10,6 @@ app.use(express.urlencoded({ extended: false }));
 
 app.use("/", exampleRouter);
 app.use("/api/uploadfile", uploadRouter);
+app.use("/api/downloadfile", downloadRouter);
 
 module.exports = app;

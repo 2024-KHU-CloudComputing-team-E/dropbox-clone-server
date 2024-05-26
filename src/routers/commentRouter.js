5091b92d49381090f661687b59a6277b42fdf859
sam_ryong <zackinthebox@khu.ac.kr>
2024-05-26 15:45:20 +0900

    CommentRouter

--- src/routers/commentRouter.js
+++ src/routers/commentRouter.js
@@ -1,8 +1,12 @@
 import express from "express";
+import commentController from "../controllers/commentController";
+import auth from "../middleware/auth";
 
 const commentRouter = express.Router();
 
-commentRouter.post("/create", async () => {});
+commentRouter.post("/create", auth, async (req, res) => {
+  commentController();
+});
 
 commentRouter.get("/read", async () => {});
 

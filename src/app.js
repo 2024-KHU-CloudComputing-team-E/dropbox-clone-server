e615dfaf0499b4d399af511b192d8969e0f43bfa
SamRyong <zackinthebox@khu.ac.kr>
2024-05-31 10:36:03 +0900

    merging...

--- src/app.js
+++ src/app.js
@@ -1,35 +1,15 @@
-//express 애플리케이션을 설정하는 파일
 import express from "express";
-import cors from "cors";
+import exampleRouter from "./routers/exampleRouter.js";
 import uploadRouter from "./routers/uploadRouter.js";
-import downloadRouter from "./routers/downloadRouter.js";
-import gooogleRouter from "./routers/googleRouter.js";
-import logoutRouter from "./routers/logoutRouter.js";
-import commentRouter from "./routers/commentRouter.js";
-import connectDB from "./config/mongodb.js";
-
-import authTestRouter from "./routers/authTestRouter.js";
+import deleteRouter from "./routers/deleteRouter.js";
 
 const app = express();
 
-app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-// 데이터베이스 연결 이거 수정해야함
-connectDB()
-  .then(() => {
-    console.log("데이터베이스 연결 성공");
-  })
-  .catch((err) => {
-    console.error("데이터베이스 연결 실패:", err);
-  });
+app.use("/", exampleRouter);
+app.use("/api/uploadfile", uploadRouter);
+app.use("/api/deletefile", deleteRouter);
 
-app.use("/api/upload", uploadRouter);
-app.use("/api/downloadfile", downloadRouter);
-app.use("/api/login", gooogleRouter);
-app.use("/api/logout", logoutRouter);
-app.use("/api/comment", commentRouter);
-app.use("/test", authTestRouter);
-
-export default app;
+module.exports = app;

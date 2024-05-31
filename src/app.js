77720f48a92a1ab596cf971cfe7ce291838fc0c3
SamRyong <zackinthebox@khu.ac.kr>
2024-05-31 11:11:44 +0900

    merging...

--- src/app.js
+++ src/app.js
@@ -1,15 +1,23 @@
 import express from "express";
-import exampleRouter from "./routers/exampleRouter.js";
 import uploadRouter from "./routers/uploadRouter.js";
 import deleteRouter from "./routers/deleteRouter.js";
+import commentRouter from "./routers/commentRouter.js";
+import downloadRouter from "./routers/downloadRouter.js";
+import googleRouter from "./routers/googleRouter.js";
+import logoutRouter from "./routers/logoutRouter.js";
+import memberRouter from "./routers/memberRouter.js";
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use("/", exampleRouter);
 app.use("/api/uploadfile", uploadRouter);
 app.use("/api/deletefile", deleteRouter);
+app.use("/api/comment", commentRouter);
+app.use("/api/downloadfile", downloadRouter);
+app.use("/api/login", googleRouter);
+app.use("/api/logout", logoutRouter);
+app.use("/api/member", memberRouter);
 
 module.exports = app;

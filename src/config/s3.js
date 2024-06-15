b78418ccb1de2e3c5ee97dcd0bd68573135efcb1
SamRyong <zackinthebox@khu.ac.kr>
2024-06-16 04:51:53 +0900

    config region edit

--- src/config/s3.js
+++ src/config/s3.js
@@ -1,7 +1,7 @@
 import { S3Client } from "@aws-sdk/client-s3";
 
 const s3 = new S3Client({
-  region: "us-east-1",
+  region: "ap-northeast-2",
   credentials: {
     accessKeyId: process.env.S3_ACCESSKEYID,
     secretAccessKey: process.env.S3_SECRETACCESSKEY,

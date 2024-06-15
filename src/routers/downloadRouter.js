88580ce4440ca5ed04ecf4c248a5e41100aea3b8
SamRyong <zackinthebox@khu.ac.kr>
2024-06-15 21:47:47 +0900

    file Download

--- src/routers/downloadRouter.js
+++ src/routers/downloadRouter.js
@@ -1,9 +1,17 @@
 import express from "express";
+import File from "../schemas/file.js";
+import { Types } from "mongoose";
+const ObjectId = Types.ObjectId;
 
 const downloadRouter = express.Router();
 
 downloadRouter.get("/", async (req, res) => {
-  console.log(req.query);
+  const fileId = req.query.fileId;
+  console.log("fileID 내노셈");
+  const objectId = new ObjectId(fileId);
+  const file = await File.findOne({ _id: objectId });
+  console.log(file.url);
+  res.send({ fileUrl: file.url });
 });
 
 export default downloadRouter;

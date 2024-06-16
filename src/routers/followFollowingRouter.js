15fb4ebaabd2bcde91ce2c277b8c899321ff3c3f
SamRyong <zackinthebox@khu.ac.kr>
2024-06-16 22:27:44 +0900

    cookie

--- src/routers/followFollowingRouter.js
+++ src/routers/followFollowingRouter.js
@@ -1,4 +1,7 @@
 import express from "express";
+import auth from "../middleware/auth.js";
+import cookieParser from "cookie-parser";
+
 import {
   follow,
   unfollow,
@@ -6,6 +9,8 @@ import {
   followerCount,
 } from "../controllers/followFollowingController.js";
 const followFollowingRouter = express.Router();
+
+followFollowingRouter.use(cookieParser());
 
 followFollowingRouter.post("/follow/:userId", auth, follow);
 followFollowingRouter.post("/unfollow/:userId", auth, unfollow);

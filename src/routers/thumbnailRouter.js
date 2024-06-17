c4bfc2772a1d641fca3ef9c7e59c359cbebb9e7c
SamRyong <zackinthebox@khu.ac.kr>
2024-06-17 19:27:21 +0900

    fileName consolelog

--- src/routers/thumbnailRouter.js
+++ src/routers/thumbnailRouter.js
@@ -5,6 +5,7 @@ const thumbnailRouter = express.Router();
 
 thumbnailRouter.get("/:fileName", (req, res) => {
   const fileName = Buffer.from(req.params.fileName, "latin1").toString("utf8");
+  console.log(fileName);
   res.sendFile(path.join(__dirname, `./thumbnails/${fileName}`));
 });
 

16b890bfa0c2e8081aa0a369125abe300925b2db
SamRyong <zackinthebox@khu.ac.kr>
2024-06-17 19:50:33 +0900

    g

--- src/app.js
+++ src/app.js
@@ -10,7 +10,7 @@ dotenv.config();
 
 app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 
 const startServer = async () => {
   try {

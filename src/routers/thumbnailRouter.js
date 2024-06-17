398f00aca5b3ef529fd4fc5e911033a81a51009e
SamRyong <zackinthebox@khu.ac.kr>
2024-06-17 19:39:52 +0900

    g

--- src/routers/thumbnailRouter.js
+++ src/routers/thumbnailRouter.js
@@ -4,9 +4,13 @@ const __dirname = path.resolve();
 const thumbnailRouter = express.Router();
 
 thumbnailRouter.get("/:fileName", (req, res) => {
-  const fileName = Buffer.from(req.params.fileName, "latin1").toString("utf8");
+  const ext = path.extname(file.originalname); // 파일 확장자
+  const fileName = path.basename(
+    Buffer.from(file.originalname, "latin1").toString("utf8"),
+    ext
+  );
   console.log(fileName);
-  res.sendFile(path.join(__dirname, `./thumbnails/${fileName}`));
+  res.sendFile(path.join(__dirname, `./thumbnails/${fileName + ext}`));
 });
 
 export default thumbnailRouter;

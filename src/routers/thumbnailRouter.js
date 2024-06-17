609c76afc24ad1a105fe040064117cfc2377e1df
SamRyong <zackinthebox@khu.ac.kr>
2024-06-17 10:17:43 +0900

    ags123

--- src/routers/thumbnailRouter.js
+++ src/routers/thumbnailRouter.js
@@ -3,7 +3,6 @@ import path from "path";
 const __dirname = path.resolve();
 const thumbnailRouter = express.Router();
 
-thumbnailRouter.get("/", getUserImages);
 thumbnailRouter.get("/:fileId", (req, res) => {
   const fileId = req.params.fileId;
   res.sendFile(path.join(__dirname, `./thumbnails/${fileId}.jpg`));

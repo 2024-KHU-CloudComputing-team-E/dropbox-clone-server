4ef4adc93962e92ea0c802ed5b5590445a0ed336
SamRyong <zackinthebox@khu.ac.kr>
2024-06-17 21:07:00 +0900

    g

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -2,19 +2,23 @@ import User from "../schemas/user.js";
 import jwt from "jsonwebtoken";
 
 let auth = async (req, res, next) => {
-  const token = req.cookies.x_auth;
+  try {
+    const token = req.cookies.x_auth;
 
-  if (!token) return res.json({ isAuth: false, error: "no token" });
-  const decoded = jwt.verify(token, "jwtSecret");
-  if (!decoded.user)
-    return res.json({ isAuth: false, error: "no decoded.user" });
-  const user = await User.findOne(decoded.user);
-  if (!user) {
-    return res.json({ isAuth: false, error: true });
-  } else {
-    req.token = user.token;
-    req.user = user;
-    next();
+    if (!token) return res.json({ isAuth: false, error: "no token" });
+    const decoded = jwt.verify(token, "jwtSecret");
+    if (!decoded.user)
+      return res.json({ isAuth: false, error: "no decoded.user" });
+    const user = await User.findOne(decoded.user);
+    if (!user) {
+      return res.json({ isAuth: false, error: true });
+    } else {
+      req.token = user.token;
+      req.user = user;
+      next();
+    }
+  } catch (err) {
+    res.send(err);
   }
 };
 

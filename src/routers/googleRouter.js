25d537d8ba45ddf238454da8aece02303cfe7123
SamRyong <zackinthebox@khu.ac.kr>
2024-06-19 20:59:44 +0900

    최종 에러 컨트롤

--- src/routers/googleRouter.js
+++ src/routers/googleRouter.js
@@ -6,18 +6,26 @@ import googleController from "../controllers/googleController.js";
 const googleRouter = express.Router();
 
 googleRouter.get("/google", (req, res) => {
-  const url = googleController.getGoogleAuthURL();
-  return res.redirect(url);
+  try {
+    const url = googleController.getGoogleAuthURL();
+    return res.redirect(url);
+  } catch (err) {
+    res.send(err);
+  }
 });
 
 googleRouter.get("/google/redirect", async (req, res) => {
-  const { code } = req.query;
-  const token = await googleController.getToken(code);
-  const userinfo = await googleController.getUserinfoByToken(
-    token.access_token
-  );
-  const user = await googleController.verifyUser(userinfo);
-  res.cookie("x_auth", user.token).redirect(`/${user.userId}`);
+  try {
+    const { code } = req.query;
+    const token = await googleController.getToken(code);
+    const userinfo = await googleController.getUserinfoByToken(
+      token.access_token
+    );
+    const user = await googleController.verifyUser(userinfo);
+    res.cookie("x_auth", user.token).redirect(`/${user.userId}`);
+  } catch (err) {
+    res.send(err);
+  }
 });
 
 export default googleRouter;

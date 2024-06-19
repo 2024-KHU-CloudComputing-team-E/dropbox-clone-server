40bc2d8e5bba734bd430c0fe9a9ad810114ea3ec
SamRyong <zackinthebox@khu.ac.kr>
2024-06-19 20:18:24 +0900

    auth

--- src/routers/deleteRouter.js
+++ src/routers/deleteRouter.js
@@ -5,13 +5,18 @@ import {
   deleteFileAndDocumentAll,
   restore,
 } from "../controllers/deleteController.js";
-//import cookieParser from "cookie-parser";
+import cookieParser from "cookie-parser";
+import auth from "../middleware/auth.js";
 const deleteRouter = express.Router();
-//deleteRouter.use(cookieParser);
+deleteRouter.use(cookieParser());
 
-deleteRouter.post("/moveFileToRecycleBin/:fileId", moveFileToRecycleBin);
-deleteRouter.post("/deleteFileOnRecycleBin/:fileId", deleteFileOnRecycleBin);
-deleteRouter.post("/deleteAll", deleteFileAndDocumentAll);
-deleteRouter.post("/restore/:fileId", restore);
+deleteRouter.post("/moveFileToRecycleBin/:fileId", auth, moveFileToRecycleBin);
+deleteRouter.post(
+  "/deleteFileOnRecycleBin/:fileId",
+  auth,
+  deleteFileOnRecycleBin
+);
+deleteRouter.post("/deleteAll", auth, deleteFileAndDocumentAll);
+deleteRouter.post("/restore/:fileId", auth, restore);
 
 export default deleteRouter;
